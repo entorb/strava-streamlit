dae5af72d7823f1632bbdd8360cab826038ec29d
Torben <59419684+entorb@users.noreply.github.com>
2024-12-25 15:34:02 +0100

    token refresh and deauth

--- src/helper_api.py
+++ src/helper_api.py
@@ -13,22 +13,60 @@ from helper_logging import init_logger
 logger = init_logger(__file__)
 
 API_RETRIES = 2
+URL_OAUTH = "https://www.strava.com/api/v3/oauth/token"
+URL_BASE = "https://www.strava.com/api/v3"
 # only used for local development to prevent api calls
 DIR_CACHE = Path("./cache/")
 
 
+def api_post_oauth(code: str) -> dict:
+    """Post the code from the oauth2 redirect to retrieve token."""
+    d = {
+        "client_id": st.secrets["client_id"],
+        "client_secret": st.secrets["secret"],
+        "code": code,
+        "grant_type": "authorization_code",
+    }
+    resp = requests.post(URL_OAUTH, json=d, timeout=15)
+    # st.write(resp.json())
+    resp.raise_for_status()
+    return resp.json()
+
+
+def api_post_token_refresh() -> dict:
+    """Refresh the oauth2 token."""
+    d = {
+        "client_id": st.secrets["client_id"],
+        "client_secret": st.secrets["secret"],
+        "grant_type": "refresh_token",
+        "refresh_token": st.session_state["TOKEN_REFRESH"],
+    }
+    resp = requests.post(URL_OAUTH, json=d, timeout=15)
+    # st.write(resp.text)
+    resp.raise_for_status()
+    return resp.json()
+
+
+def api_post_deauthorize() -> None:
+    """Deauthorize this app from user's strava account."""
+    headers = {"Authorization": f"Bearer {st.session_state['TOKEN']}"}
+    resp = requests.post(
+        "https://www.strava.com/oauth/deauthorize", headers=headers, timeout=15
+    )
+    resp.raise_for_status()
+
+
 # not caching raw data
-def _api_get(url: str) -> dict | list:
+def _api_get(path: str) -> dict | list:
     """Get data from Strava API."""
-    baseurl = "https://www.strava.com/api/v3"
-    url = f"{baseurl}/{url}"
-    logger.info(url)
+    path = f"{URL_BASE}/{path}"
+    logger.info(path)
 
     headers = {"Authorization": f"Bearer {st.session_state['TOKEN']}"}
 
     for attempt in range(API_RETRIES):  # Try once, then retry once if it fails
         try:
-            resp = requests.get(url, headers=headers, timeout=(3, 30))
+            resp = requests.get(path, headers=headers, timeout=(3, 30))
             # Raise HTTPError if HTTP request returns an unsuccessful status code
             resp.raise_for_status()
             return resp.json()
@@ -40,30 +78,36 @@ def _api_get(url: str) -> dict | list:
     return []
 
 
-def read_cache_file(cache_file: Path) -> None | dict | list:
+def read_cache_file(cache_file: str) -> None | dict | list:
     """Read a json cache file, only used for local dev."""
-    if not cache_file.is_file():
+    p = DIR_CACHE / cache_file
+    if not p.is_file():
         return None
-    with cache_file.open(encoding="utf-8") as fh:
+    with p.open(encoding="utf-8") as fh:
         d = json.load(fh)
     return d
 
 
-def write_cache_file(cache_file: Path, d: dict | list) -> None:
+def write_cache_file(cache_file: str, d: dict | list) -> None:
     """Write a json cache file, only used for local dev."""
-    with cache_file.open("w", encoding="utf-8", newline="\n") as fh:
+    p = DIR_CACHE / cache_file
+    with p.open("w", encoding="utf-8", newline="\n") as fh:
         json.dump(d, fh, ensure_ascii=False, sort_keys=False, indent=2)
 
 
 # no caching, as only performed once upon login
 def fetch_athlete_info() -> str:
-    """Get athlete ID and username and set in session_state."""
-    cache_file = DIR_CACHE / "athlete.json"
+    """
+    Get athlete ID and username and set in session_state.
+
+    not used any more, since also included in api_post_oauth()
+    """
+    cache_file = "athlete.json"
     d = None
     if st.session_state["ENV"] == "DEV":
         d = read_cache_file(cache_file)
     if not d:
-        d = _api_get(url="athlete")
+        d = _api_get(path="athlete")
         if st.session_state["ENV"] == "DEV":
             write_cache_file(cache_file, d=d)
     assert type(d) is dict
@@ -75,14 +119,14 @@ def fetch_athlete_info() -> str:
 # not caching this raw data
 def fetch_activities_page(page: int) -> list[dict]:
     """Request a page of 200 activities."""
-    cache_file = DIR_CACHE / f"activities-page-{page}.json"
+    cache_file = f"activities-page-{page}.json"
     lst = None
     current_timestamp = int(time())
     if st.session_state["ENV"] == "DEV":
         lst = read_cache_file(cache_file)
-    if not lst:
+    if lst is None:
         lst = _api_get(
-            url=f"athlete/activities?per_page=200&page={page}&before={current_timestamp}&after=0"
+            path=f"athlete/activities?per_page=200&page={page}&before={current_timestamp}&after=0"
         )
         if st.session_state["ENV"] == "DEV":
             write_cache_file(cache_file, d=lst)
@@ -93,31 +137,13 @@ def fetch_activities_page(page: int) -> list[dict]:
 @st.cache_data(ttl="5m")
 def fetch_gear_data(gear_id: int) -> dict:
     """Fetch gear info and return name."""
-    cache_file = DIR_CACHE / f"gear-{gear_id}.json"
+    cache_file = f"gear-{gear_id}.json"
     d = None
     if st.session_state["ENV"] == "DEV":
         d = read_cache_file(cache_file)
     if not d:
-        d = _api_get(url=f"gear/{gear_id}")
+        d = _api_get(path=f"gear/{gear_id}")
         if st.session_state["ENV"] == "DEV":
             write_cache_file(cache_file, d=d)
     assert type(d) is dict
     return d
-
-
-# not caching this raw data
-def fetch_all_activities() -> list[dict]:
-    """Loop over fetch_activities_page unless the result is empty."""
-    page = 1
-    lst_all_activities = []
-    while True:
-        # st.write(f"Downloading page {page}")
-        lst = fetch_activities_page(page=page)
-        if len(lst) == 0:
-            break
-        lst_all_activities.extend(lst)
-        page += 1
-        # # at dev, only download page 1
-        # if st.session_state["ENV"] == "DEV":
-        #     break
-    return lst_all_activities
