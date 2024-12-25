9ee3a571bc7f66419d110d9e83f5fe5121400ef0
Torben <59419684+entorb@users.noreply.github.com>
2024-12-25 15:34:05 +0100

    pytest, testdata and GH action workflow

---
+++ tests/helper_activities_caching_test.py
@@ -0,0 +1,83 @@
+# ruff: noqa: D100 D103 INP001 PLR2004 S101
+
+
+import sys
+from pathlib import Path
+
+import streamlit as st
+from streamlit.testing.v1 import AppTest
+
+sys.path.insert(0, (Path(__file__).parent.parent / "src").as_posix())
+from helper_activities_caching import (
+    cache_all_activities_and_gears,
+    check_is_known_location,
+    cities_into_1deg_geo_boxes,
+    fetch_all_activities,
+    geo_distance_haversine,
+    read_city_db,
+    reduce_geo_precision,
+    search_closest_city,
+)
+
+st.session_state["ENV"] = "DEV"
+st.session_state["USER_ID"] = 7656541
+
+at = AppTest.from_file("src/helper_activities_caching.py")
+
+hamburg = (53.5715, 10.0110)
+munich = (48.1492, 11.5860)
+
+
+def test_reduce_geo_precision() -> None:
+    coord = (53.5665, 10.0110)
+    reduced_coord = reduce_geo_precision(coord, 2)
+    assert reduced_coord == (53.57, 10.01)
+
+
+def test_geo_distance_haversine() -> None:
+    print(geo_distance_haversine(hamburg, munich))
+    assert round(geo_distance_haversine(hamburg, munich), 1) == 612.9
+
+
+def test_check_is_known_location() -> None:
+    # cspell:disable-next-line
+    assert check_is_known_location((49.59, 11.03)) == "ER-ObiKreisel"
+
+
+def test_fetch_all_activities() -> None:
+    _ = fetch_all_activities()
+    assert not at.exception
+
+
+def test_read_city_db() -> None:
+    lst = read_city_db()
+    assert len(lst) == 4
+
+
+def test_cities_into_1deg_geo_boxes() -> None:
+    boxes = cities_into_1deg_geo_boxes()
+    assert len(boxes) == 16
+
+
+def test_search_closest_city() -> None:
+    assert search_closest_city((53.5, 10.0)) == "EU-DE-HH-Hamburg"
+
+
+def test_cache_all_activities_and_gears() -> None:
+    df, df_gear = cache_all_activities_and_gears()
+    assert not at.exception
+
+
+def test_cache_all_activities_and_gears_2() -> None:
+    df, df_gear = cache_all_activities_and_gears()
+    # run
+    assert df["x_km"].iat[0] == 13.072
+    assert round(df["x_dist_start_end_km"].iat[0], 3) == 3.391
+    # ride
+    assert df["x_nearest_city_start"].iat[1] == "EU-DE-SN-Dresden"
+    assert df["x_mi"].iat[1] == 18.926
+    # swim
+    assert df["x_elev_%"].iat[2] == 0
+
+
+df, df_gear = cache_all_activities_and_gears()
