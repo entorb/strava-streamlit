fe73996f0090ec8d33ae6d85b1fe03e7595b0a13
Torben <59419684+entorb@users.noreply.github.com>
2024-12-27 13:21:31 +0100

    add strava-resources logos

--- src/helper_login.py
+++ src/helper_login.py
@@ -13,8 +13,15 @@ logger = get_logger_from_filename(__file__)
 def display_strava_auth_link() -> None:
     """Display link for Strava auth."""
     st.title("Login")
+    # TODO: using button image from App V1
     st.write(
-        "<a target='_self' href='http://www.strava.com/oauth/authorize?client_id=28009&response_type=code&redirect_uri=https://entorb.net/strava-streamlit/?exchange_token&approval_prompt=force&scope=activity:read_all'>Login</a>",
+        """
+<a target='_self' href='http://www.strava.com/oauth/authorize?client_id=28009&response_type=code&redirect_uri=https://entorb.net/strava-streamlit/?exchange_token&approval_prompt=force&scope=activity:read_all'>
+<button class="strava-connect-button">
+<img src="/strava/strava-resources/btn_strava_connectwith_light.svg" alt="Connect with Strava">
+</button>
+</a>
+""",
         unsafe_allow_html=True,
     )
 
