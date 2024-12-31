0815394c6763b67d28cf80c801923866f5d21057
Torben <59419684+entorb@users.noreply.github.com>
2024-12-31 11:48:23 +0100

    track_function_usage()

--- src/helper_logging.py
+++ src/helper_logging.py
@@ -1,11 +1,44 @@
 """Helper: Logging."""
 
+import time
+from collections import defaultdict
+from collections.abc import Callable
+from functools import wraps
 from logging import Logger
 from pathlib import Path
 
+import streamlit as st
 from streamlit.logger import get_logger
 
 
 def get_logger_from_filename(file: str) -> Logger:
     """Return logger using filename name."""
     return get_logger(Path(file).stem)
+
+
+# Dictionary to store metrics
+@st.cache_resource
+def get_call_stats() -> defaultdict[str, dict[str, int | float]]:
+    """Create cached dict of call stats."""
+    return defaultdict(lambda: {"calls": 0, "total_time": 0.0})
+
+
+def track_function_usage(func: Callable) -> Callable:
+    """Annotation for gathering runtime statistics."""
+
+    @wraps(func)
+    def wrapper(*args: tuple, **kwargs: dict):  # noqa: ANN202
+        # Gather stats only for me
+        if st.session_state["USER_ID"] == st.secrets["my_user_id"]:
+            start_time = time.time()
+            result = func(*args, **kwargs)  # Call the original function
+            end_time = time.time()
+            call_stats = get_call_stats()
+            call_stats[func.__name__]["calls"] += 1
+            call_stats[func.__name__]["total_time"] += end_time - start_time
+            return result
+
+        # normal user
+        return func(*args, **kwargs)  # Call the original function
+
+    return wrapper
--- src/reports/98_Internal_Stats.py
+++ src/reports/98_Internal_Stats.py
@@ -1,14 +1,27 @@
 """Internal Stats."""
 
+import pandas as pd
 import streamlit as st
 
-from helper_logging import get_logger_from_filename
+from helper_logging import get_call_stats, get_logger_from_filename
 
 st.title(__doc__[:-1])  # type: ignore
 
 logger = get_logger_from_filename(__file__)
 logger.info("Start")
+
+# double check, that this file is only access-able by me
+if st.session_state["USER_ID"] != st.secrets["my_user_id"]:
+    st.stop()
+
 st.header("Session State")
 st.write(st.session_state)
+
+
+st.header("Fct Call Stats")
+call_stats = get_call_stats()
+df = pd.DataFrame(call_stats).T
+df["total_time"] = df["total_time"].round(3)
+st.dataframe(df, hide_index=False)
 
 logger.info("End")
