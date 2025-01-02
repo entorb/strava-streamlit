be4e477506faf37351eeeaaf2f9da7e5a7285340
Torben <59419684+entorb@users.noreply.github.com>
2025-01-02 15:11:42 +0100

    Ensure all columns are there

--- src/helper_pandas.py
+++ src/helper_pandas.py
@@ -10,10 +10,11 @@ logger = get_logger_from_filename(__file__)
 def reorder_cols(df: pd.DataFrame, col_first: list[str]) -> pd.DataFrame:
     """Reorder DataFrame columns, put col_first to beginning."""
     cols = df.columns.to_list()
+    for col in reversed(col_first):
+        if col not in cols:
+            col_first.remove(col)
     for col in col_first:
         if col in cols:
             cols.remove(col)
-        else:
-            logger.warning(f"'{col}' not in columns: {df.columns.to_list()} ")  # noqa: G004
     col_first.extend(cols)
     return df[col_first]
