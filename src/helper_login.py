d10c1be4b995a76347587b456befeb0c5ade5b22
Torben <59419684+entorb@users.noreply.github.com>
2025-01-05 08:10:34 +0100

    Update helper_login.py

--- src/helper_login.py
+++ src/helper_login.py
@@ -1,5 +1,7 @@
 """Helper: Login to Strava via OAuth2."""
 
+# ruff: noqa: E501
+
 from time import time
 
 import streamlit as st
@@ -18,16 +20,33 @@ logger = get_logger_from_filename(__file__)
 def display_strava_auth_link() -> None:
     """Display link for Strava auth."""
     st.title("Login")
-    # TODO: using button image from App V1
+
+    # this text is copied from README.md
+    st.markdown("""This is a modern rewrite of my [old Strava Äpp](https://entorb.net/strava/). Currently, only read-only statistics features are implemented. For bulk modify of activities and Excel import, please use the old Äpp.
+""")
+
+    # TODO: currently re-using button image from App V1
     st.write(
         """
-<a target='_self' href='http://www.strava.com/oauth/authorize?client_id=28009&response_type=code&redirect_uri=https://entorb.net/strava-streamlit/?exchange_token&approval_prompt=force&scope=activity:read_all'>
+<a target='_self' href='https://www.strava.com/oauth/authorize?client_id=28009&response_type=code&redirect_uri=https://entorb.net/strava-streamlit/?exchange_token&approval_prompt=force&scope=activity:read_all'>
 <button class="strava-connect-button">
 <img src="/strava/strava-resources/btn_strava_connectwith_light.svg" alt="Connect with Strava">
 </button>
 </a>
-""",  # noqa: E501
+""",
         unsafe_allow_html=True,
+    )
+
+    # this text is copied from README.md
+    st.markdown(
+        """
+## Privacy
+
+* **Code:** The [source-code](https://github.com/entorb/strava-streamlit/) is open source.
+* **Data:** The app does not use a database. Your Strava data is only temporarily cached.
+* **Access:** A temporary access token to your Strava profile is used and revoked at logout.
+* **Cookies:** Only a single technical cookie is used for session identification and deleted at end of session. No user tracking.
+"""
     )
 
 
