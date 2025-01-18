f3a24837744e40ae1cc3a957c15a161ff4b8c95d
Torben <59419684+entorb@users.noreply.github.com>
2025-01-18 18:30:54 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ rsync -uz .streamlit/config-prod.toml entorb@entorb.net:strava-streamlit/.stream
 rsync -uz .streamlit/secrets.toml entorb@entorb.net:strava-streamlit/.streamlit/secrets.toml
 rsync -uz requirements.txt entorb@entorb.net:strava-streamlit/
 rsync -uz activity_columns.txt entorb@entorb.net:strava-streamlit/
-rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ --exclude 99_Playground.py src/ entorb@entorb.net:strava-streamlit/src/
+rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ --exclude r99_Playground.py src/ entorb@entorb.net:strava-streamlit/src/
 
 echo installing packages
 ssh entorb@entorb.net "pip3.11 install --user streamlit -r strava-streamlit/requirements.txt > /dev/null"
