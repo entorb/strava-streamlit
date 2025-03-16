bd0cd899d5194d8c723c998668a63bfde69e8cca
Torben <59419684+entorb@users.noreply.github.com>
2025-03-16 06:07:52 +0100

    Download-orig-link, SQ findings

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd $(dirname $0)/..
 
+# my webserver uses 3.11, so I use it for local dev too.
+pyenv global 3.11
 streamlit run src/app.py
 # for production better use
 # python3.11 -O -m streamlit run src/app.py
+pyenv global 3.12
