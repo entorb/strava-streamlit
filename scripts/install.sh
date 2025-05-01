ea80eebdc2102ad598d2a6a6361676c72d977678
Torben <59419684+entorb@users.noreply.github.com>
2025-05-01 16:47:05 +0200

    revert to requirements.txt

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,10 +5,9 @@ pyenv global 3.11
 # eval "$(pyenv init -)"
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-pip install -r requirements-dev.txt
+# pip install -r requirements-dev.txt
 pip freeze >requirements-all.txt
 
-# to upgrade
 # pip install --upgrade -r requirements.txt
 # pip install --upgrade -r requirements-dev.txt
 
