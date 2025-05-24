5d25f22f6b7629383fd545d80e02c7dfa6c40aaa
Torben <59419684+entorb@users.noreply.github.com>
2025-05-24 09:41:34 +0200

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,9 @@ cd $(dirname $0)/..
 
 pyenv global 3.11
 python -m pip install --upgrade pip
-pip install --upgrade -r streamlit XlsxWriter
+pip install --upgrade streamlit XlsxWriter
 pip freeze >requirements-all.txt
-grep -E "streamlit|XlsxWriter" requirements-all.txt >requirements.txt
+grep -E "streamlit=|XlsxWriter=" requirements-all.txt >requirements.txt
+
+# update pyproject.toml
+uv add -r requirements.txt
