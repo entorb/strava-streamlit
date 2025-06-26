8605ec65bff82b867a9097cc902d3cea2973a70b
Torben <59419684+entorb@users.noreply.github.com>
2025-06-26 07:05:02 +0200

    Add Sentry

--- src/app.py
+++ src/app.py
@@ -16,13 +16,34 @@ from helper_login import (
 )
 from helper_ui_components import create_navigation_menu
 
-logger = get_logger_from_filename(__file__)
-logger.info("Start")
+
+def set_env() -> None:
+    """Set ENV to entorb.net / local."""
+    if "ENV" not in st.session_state:
+        if Path("/home/entorb/strava-streamlit").is_dir():
+            st.session_state["ENV"] = "PROD"
+        else:
+            st.session_state["ENV"] = "DEV"
+            # when running locally, ensure we have data dirs
+            Path("./cache").mkdir(exist_ok=True)
+            Path("./data").mkdir(exist_ok=True)
 
 
-def include_matomo_stats() -> None:
-    """Include Matomo access stats update JavaScript snippet."""
-    import streamlit.components.v1 as components
+def init_sentry() -> None:
+    """Initialize Sentry exception tracking/alerting."""
+    import sentry_sdk  # noqa: PLC0415
+
+    sentry_sdk.init(
+        dsn=st.secrets["sentry_dns"],
+        environment=st.session_state["ENV"],
+        send_default_pii=True,
+        traces_sample_rate=0.0,
+    )
+
+
+def init_matomo() -> None:
+    """Initialize Matomo access stats, via JavaScript snippet."""
+    import streamlit.components.v1 as components  # noqa: PLC0415
 
     components.html(
         """
@@ -43,24 +64,8 @@ _paq.push(['enableLinkTracking']);
     )
 
 
-def set_env() -> None:
-    """Set ENV to entorb.net / local."""
-    if "ENV" not in st.session_state:
-        if Path("/home/entorb/strava-streamlit").is_dir():
-            st.session_state["ENV"] = "PROD"
-        else:
-            st.session_state["ENV"] = "DEV"
-            # when running locally, ensure we have data dirs
-            Path("./cache").mkdir(exist_ok=True)
-            Path("./data").mkdir(exist_ok=True)
-
-
-set_env()
-if st.session_state["ENV"] == "PROD":
-    include_matomo_stats()
-
-# for local development I skip the login
-if st.session_state["ENV"] == "DEV":
+def init_dev_session_state() -> None:
+    """Set session variables needed for local dev without login."""
     st.session_state["TOKEN"] = st.secrets["my_token"]
     st.session_state["TOKEN_EXPIRE"] = int(time() + 24 * 3600)
     st.session_state["TOKEN_REFRESH"] = st.secrets["my_refresh_token"]
@@ -71,16 +76,42 @@ if st.session_state["ENV"] == "DEV":
     )
     st.session_state["USERNAME"] = st.secrets["my_username"]
 
-if "TOKEN" not in st.session_state:
-    perform_login()
+
+logger = get_logger_from_filename(__file__)
+logger.info("Start")
 
 
-if "TOKEN" in st.session_state:
-    # check if we need to refresh the token
-    token_refresh_if_needed()
+def main() -> None:  # noqa: D103
+    set_env()
 
-    st.logo("src/strava-resources/api_logo_pwrdBy_strava_stack_light.svg", size="large")
-    create_navigation_menu()
+    if st.session_state["ENV"] == "PROD":
+        init_sentry()
+        init_matomo()
+
+    if st.session_state["ENV"] == "DEV":
+        init_dev_session_state()
+
+    if "TOKEN" not in st.session_state:
+        perform_login()
+
+    if "TOKEN" in st.session_state:
+        # check if we need to refresh the token
+        token_refresh_if_needed()
+
+        st.logo(
+            "src/strava-resources/api_logo_pwrdBy_strava_stack_light.svg", size="large"
+        )
+        create_navigation_menu()
+
+    logger.info("End")
 
 
-logger.info("End")
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        # automatically triggered by logger.exception
+        # sentry_sdk.capture_exception(e)
+        logger.exception("Exception:")
+        st.exception(e)
+        st.stop()
