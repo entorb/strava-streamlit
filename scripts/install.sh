e7ce9dfa9af51d1c5024dc7bbeedc04ea48e0b2f
Torben <59419684+entorb@users.noreply.github.com>
2025-06-27 20:53:40 +0200

    uv in install and update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 cd $(dirname $0)/..
 
-pyenv global 3.11
+uv lock --upgrade
+uv sync --upgrade
+
+# extract package info to requirements.txt
+# uv export --format requirements.txt --no-dev --no-hashes -o requirements.txt
+python scripts/gen_requirements.py
+
+# pyenv global 3.11
+
 # eval "$(pyenv init -)"
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-# pip install -r requirements-dev.txt
-pip freeze >requirements-all.txt
+# python -m pip install --upgrade pip
+# pip install -r requirements.txt
+# # pip install -r requirements-dev.txt
+# pip freeze >requirements-all.txt
 
 # pip install --upgrade -r requirements.txt
 # pip install --upgrade -r requirements-dev.txt
 
-pyenv global 3.13
+# pyenv global 3.13
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 cd $(dirname $0)/..
 
-pyenv global 3.11
-python -m pip install --upgrade pip
-pip install --upgrade streamlit XlsxWriter
-pip freeze >requirements-all.txt
-grep -E "streamlit=|XlsxWriter=" requirements-all.txt >requirements.txt
+# pyenv global 3.11
+# python -m pip install --upgrade pip
+# pip install --upgrade streamlit XlsxWriter
+# pip freeze >requirements-all.txt
+# grep -E "streamlit=|XlsxWriter=" requirements-all.txt >requirements.txt
 
 # update pyproject.toml
-uv add -r requirements.txt
+# uv add -r requirements.txt
+# pyenv global 3.13
+
+uv remove pandas sentry-sdk streamlit XlsxWriter
+uv remove --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
+
+uv lock --upgrade
+uv sync --upgrade
+
+uv add pandas sentry-sdk streamlit XlsxWriter
+uv add --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
+
+uv lock --upgrade
+uv sync --upgrade
+
+python scripts/gen_requirements.py
