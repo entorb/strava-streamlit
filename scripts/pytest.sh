81d34bc416fa929c18bb4b7fd8dac4c013a09b9d
Torben <59419684+entorb@users.noreply.github.com>
2025-06-27 21:15:22 +0200

    run via uv

--- scripts/pytest.sh
+++ scripts/pytest.sh
@@ -2,4 +2,5 @@
 cd $(dirname $0)/..
 
 sh scripts/copy_test_data.sh
-sh pytest --cov --cov-report=html:coverage_report
+uv run pytest --cov --cov-report=html:coverage_report
+# sh pytest --cov --cov-report=html:coverage_report
