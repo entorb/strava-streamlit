2388af86fe584a28f1003d21be8c6d2b7ad42275
Torben <59419684+entorb@users.noreply.github.com>
2025-10-19 16:17:19 +0200

    Scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+cd $(dirname $0)/..
+
+# exit upon error
+set -e
+
+uv run ruff check
+uv run ruff format
+
+sh scripts/pytest.sh
+
+echo copying
+# config.toml -> config-prod.toml
+python3 scripts/config_convert.py
+rsync -uz .streamlit/config-prod.toml entorb@entorb.net:strava-streamlit/.streamlit/config.toml
+rsync -uz .streamlit/secrets.toml entorb@entorb.net:strava-streamlit/.streamlit/secrets.toml
+rsync -uz requirements.txt entorb@entorb.net:strava-streamlit/
+rsync -uz activity_columns.txt entorb@entorb.net:strava-streamlit/
+rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ --exclude r99_Playground.py src/ entorb@entorb.net:strava-streamlit/src/
+
+echo installing packages
+ssh entorb@entorb.net "pip3.11 install --user -r strava-streamlit/requirements.txt > /dev/null"
+
+echo restarting strava-streamlit
+ssh entorb@entorb.net "supervisorctl restart strava-streamlit"
+
+echo DONE
---
+++ scripts/update.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+cd $(dirname $0)/..
+
+# pyenv global 3.11
+# python -m pip install --upgrade pip
+# pip install --upgrade streamlit XlsxWriter
+# pip freeze >requirements-all.txt
+# grep -E "streamlit=|XlsxWriter=" requirements-all.txt >requirements.txt
+
+# update pyproject.toml
+# uv add -r requirements.txt
+# pyenv global 3.13
+
+uv remove pandas pyarrow sentry-sdk streamlit XlsxWriter
+uv remove --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
+
+uv lock --upgrade
+uv sync --upgrade
+
+uv add pandas==2.2.3 pyarrow==20.0.0 sentry-sdk streamlit XlsxWriter
+uv add --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
+
+uv lock --upgrade
+uv sync --upgrade
+
+python scripts/gen_requirements.py
