b1c67458be336b56c2466cfc86124f6597ce81b0
Torben <59419684+entorb@users.noreply.github.com>
2026-01-01 11:49:37 +0100

    adjust links to entorb.net/strava-old/

--- src/reports/r88_contact.py
+++ src/reports/r88_contact.py
@@ -10,5 +10,5 @@ logger = get_logger_from_filename(__file__)
 
 st.markdown("""
 * [Feedback](https://entorb.net/contact.php?origin=strava) and ideas for new reports are highly appreciated.
-* For bulk modify of activities and Excel import, please use the old [Äpp](https://entorb.net/strava/).
+* For bulk modify of activities and Excel import, please use the old [Äpp](https://entorb.net/strava-old/).
 """)  # noqa: E501
