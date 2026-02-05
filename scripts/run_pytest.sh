10acadd455e9d0554cabbc7bc559d2860d50733e
Torben <59419684+entorb@users.noreply.github.com>
2026-02-05 20:05:38 +0100

    Scripts

--- scripts/run_pytest.sh
+++ scripts/run_pytest.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+# ensure we are in the root dir
 cd $(dirname $0)/..
 
 sh scripts/copy_test_data.sh
+
 uv run pytest --cov --cov-report=html:coverage_report
 # sh pytest --cov --cov-report=html:coverage_report
