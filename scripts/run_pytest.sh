c5e8d262f4150f11db336e07601606ff5e9723d7
Torben <59419684+entorb@users.noreply.github.com>
2026-02-16 01:14:35 +0100

    Update tool config

--- scripts/run_pytest.sh
+++ scripts/run_pytest.sh
@@ -5,5 +5,4 @@ cd $(dirname $0)/..
 
 sh scripts/copy_test_data.sh
 
-uv run pytest --cov --cov-report=html:coverage_report
-# sh pytest --cov --cov-report=html:coverage_report
+uv run pytest --cov --cov-report=html:coverage_report --cov-report=term-missing
