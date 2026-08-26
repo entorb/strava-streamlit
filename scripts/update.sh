9faa4208a81b7ff76e0186bb484a34a2ba40007f
Torben <59419684+entorb@users.noreply.github.com>
2026-08-26 15:17:01 +0200

    chore: Tools

---
+++ scripts/update.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# ensure we are in the root dir
+cd $(dirname $0)/..
+
+# exit upon error
+set -e
+
+uv remove numpy pandas pyarrow sentry-sdk streamlit XlsxWriter
+uv remove --dev ruff pytest pytest-cov tomli-w vulture watchdog
+
+uv lock --upgrade
+uv sync --upgrade
+
+# pin to old versions due to Uberspace restrictions
+uv add numpy==2.2.3 pandas==2.2.3 pyarrow==20.0.0 sentry-sdk streamlit XlsxWriter
+uv add --dev ruff pytest pytest-cov tomli-w vulture watchdog
+
+uv lock --upgrade
+uv sync --upgrade
+
+python scripts/gen_requirements.py
+
+# ruff
+uv run ruff format
+uv run ruff check --fix
+
+# pre-commit
+prek autoupdate
+prek run --all-files
+
+echo DONE
