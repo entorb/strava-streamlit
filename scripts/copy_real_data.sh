4c04d8599ad16f378ff88d51dc07febe3c9ede00
Torben <59419684+entorb@users.noreply.github.com>
2026-09-20 16:16:52 +0200

    Scripts

--- scripts/copy_real_data.sh
+++ scripts/copy_real_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd "$(dirname "$0")/.."
+cd "$(dirname "$0")/.." || exit 1
 
 mkdir -p data/knownLocations
 mkdir -p cache
--- scripts/run_pre-commit.sh
+++ scripts/run_pre-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # ensure we are in the root dir
-cd "$(dirname "$0")/.."
+cd "$(dirname "$0")/.." || exit 1
 
 prek run --all-files
